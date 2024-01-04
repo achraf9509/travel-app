1af16f6db06accd406df3c72e348fb214d7673f7
belakbir achraf <belakbir.achraf@gmail.com>
2024-01-04 12:11:33 +0000

    description du changement

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    images:{unoptimized:true}
+    images:{unoptimized:true},
+    experimental:{cache:true},
+
 }
 
 module.exports = nextConfig
